bb3a795a23ddfe35fc29c05e6f0b488b69cf3f29
Sandeep03edu <sandeep03edu@gmail.com>
2021-10-14 13:09:09 +0530

    Minor change

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -45,16 +45,8 @@ public class MainActivity extends AppCompatActivity {
 //                startActivityForResult(intent, ADD_NOTE_REQUEST);
 
                 AppUpdater appUpdater = new AppUpdater(MainActivity.this)
-                        .setTitleOnUpdateAvailable("Update available")
-                        .setContentOnUpdateAvailable("Check out the latest version available of my app!")
-                        .setTitleOnUpdateNotAvailable("Update not available")
-                        .setContentOnUpdateNotAvailable("No update available. Check for updates again later!")
-                        .setButtonUpdate("Update now?")
-                        .setButtonDismiss("Maybe later")
-                        .setButtonDoNotShowAgain("Huh, not interested")
-                        .setIcon(R.mipmap.icon_launcher) // Notification icon
-                        .setDisplay(Display.DIALOG)
-                        .setCancelable(false)
+                        .setDisplay(Display.SNACKBAR)
+                        .showAppUpdated(true)
                         .setUpdateFrom(UpdateFrom.GITHUB)
                         .setGitHubUserAndRepo("Sandeep03edu", "Notepad") ;
 
